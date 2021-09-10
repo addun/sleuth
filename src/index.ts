a8a0a80ed0b2edb4f431755ec600627580b16bed
Adrian Bury <adrian.jakub.bury@gmail.com>
2021-09-10 23:01:43 +0200

    Handle events by callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("It works");
+async function pushEventsToServer(events: Event[]) {
+  return new Promise((resolve, reject) => {
+    let t = setTimeout(() => {
+      console.log("Events sent");
+      resolve(void 0);
+    }, 1000);
+  });
+}
+
+let eventsToSend: Event[] = [];
+
+async function pushEventToCache($event: Event) {
+  eventsToSend.push($event);
+  if (eventsToSend.length === 10) {
+    await pushEventsToServer(eventsToSend);
+    eventsToSend = [];
+  }
+}
+
+document.onclick = async function ($event) {
+  pushEventToCache($event);
+};
+
+document.onkeydown = async function ($event) {
+  if ($event.key !== "Enter") {
+    return;
+  }
+  pushEventToCache($event);
+};
+
+let currentTimeout = 0;
+document.onscroll = async function ($event) {
+  clearTimeout(currentTimeout);
+
+  currentTimeout = window.setTimeout(() => {
+    pushEventToCache($event);
+  }, 100);
+};
