f171dcf772afb29a52bd306a1431153f3465cddc
Adrian Bury <adrian.jakub.bury@gmail.com>
2021-09-11 14:27:52 +0200

    Add firebase to save data

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,54 @@
 import { fromEvent, merge } from "rxjs";
-import { bufferTime, debounceTime, filter, throttleTime } from "rxjs/operators";
+import {
+  bufferTime,
+  debounceTime,
+  filter,
+  map,
+  tap,
+  throttleTime,
+} from "rxjs/operators";
+import "firebase/firestore";
+import firebaseApp from "firebase/app";
 
-async function pushEventsToServer(events: Event[]) {
-  return new Promise((resolve, reject) => {
-    let t = setTimeout(() => {
-      console.log("Events sent");
-      resolve(void 0);
-    }, 1000);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBR6erskXRbcoG5_yySMLITsSbxZ_3DaA0",
+  authDomain: "sleuth-35cb9.firebaseapp.com",
+  projectId: "sleuth-35cb9",
+  storageBucket: "sleuth-35cb9.appspot.com",
+  messagingSenderId: "22877442462",
+  appId: "1:22877442462:web:cf8f3f5a25cb9304a9468f",
+};
+
+// Initialize Firebase
+const app = firebaseApp.initializeApp(firebaseConfig);
+const firestore = firebaseApp.firestore(app);
+
+function mapToUserEvent(event: Event | MouseEvent | KeyboardEvent) {
+  const data: Record<string, any> = {
+    type: event.type,
+    timestapamp: Date.now(),
+    windowX: window.scrollX,
+    windowY: window.scrollY,
+  };
+  if (event instanceof MouseEvent) {
+    data.x = event.x;
+    data.y = event.y;
+  }
+
+  if (event instanceof KeyboardEvent) {
+    data.key = event.key;
+  }
+
+  return data;
+}
+
+async function pushEventsToServer(events: any[]) {
+  const batch = firestore.batch();
+  events.forEach((event) => {
+    batch.set(firestore.collection("events").doc(), mapToUserEvent(event));
   });
+  return batch.commit();
 }
 
 const clickEvents$ = fromEvent<MouseEvent>(document, "click");
@@ -27,7 +68,8 @@ const scrollEvents$ = fromEvent<Event>(document, "scroll").pipe(
 
 merge(clickEvents$, keyDownEvents$, scrollEvents$, mouseMoveEvents$)
   .pipe(
-    bufferTime(2_500, null, 3),
+    map((event) => mapToUserEvent(event)),
+    bufferTime(20_000, null, 50),
     filter((events) => events.length > 0)
   )
   .subscribe((events) => pushEventsToServer(events));
